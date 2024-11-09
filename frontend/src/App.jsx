a51bdb08e0d7379c963c6645468601c173893d53
Arpit Verma <131401678+vermaArpitt@users.noreply.github.com>
2024-11-10 03:33:22 +0530

    login and register completed
    React and Django connection for logging in and registering completed.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import NotFound from "./pages/NotFound"
+import ProtectedRoutes from "./components/ProtectedRoutes"
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function LogoutAndRegister() {
+  localStorage.clear()
+  return <Register />
+}
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route 
+        path="/" 
+        element={
+          <ProtectedRoutes>
+            <Home/>
+          </ProtectedRoutes>
+        }/>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<LogoutAndRegister/>} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,7 @@
+import Form from "../components/Form"
+
+export default function Register() {
+    return(
+        <Form route="/api/user/register/" method="register" />
+    )
+}
